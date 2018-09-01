c810737b509c2fd39064b4b2a0a9165b7e9db3d6
Nixikaz <nixikaz@gmail.com>
2018-08-31 23:41:30 -0700

    Added bootstrap styles and input functionality for grid size.

--- etch.js
+++ etch.js
@@ -1,6 +1,38 @@
+let body = document.querySelector('body');
 
+let mainContent = document.createElement("div");
+let headerRow = document.createElement("div");
+let headerCol = document.createElement("div");
+let gridContainer = document.createElement("div");
+
+
+mainContent.setAttribute("id", "maincontent");
+mainContent.setAttribute("class", "container-fluid p-5 text-center");
+mainContent.innerHTML = `<h1 class="display-4">Etch-a-Sketch</h1>`
+
+headerRow.setAttribute("id", "headerrow");
+headerRow.setAttribute("class", "row p-2 justify-content-center");
+
+headerCol.setAttribute("id", "headercol");
+headerCol.setAttribute("class", "col-md-auto p-2");
+headerCol.innerHTML = `<div class="input-group mb-3">
+<input id="gridSizeInput" type="text" class="form-control" placeholder="Enter a grid size (16 - 128)" aria-label="Recipient's username" aria-describedby="submit">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button" id="submit">Reset</button>
+        </div>
+      </div>`;
+
+gridContainer.setAttribute("id", "gridcontainer");
+gridContainer.style.cssText = `display: flex;flex-wrap: wrap;border-radius: 10px;border: ridge 5px lightgrey;margin: 0 auto; padding: 0; height: 600px;width: 600px`;
+
+body.appendChild(mainContent);
+mainContent.appendChild(headerRow);
+headerRow.appendChild(headerCol);
+headerCol.appendChild(gridContainer);
+createGrid(16);
 
 function createGrid(gridSize) {
+    
         for (let i = 0; i < gridSize * gridSize; i++) {
             let gridBox = document.createElement("div");
             gridBox.setAttribute("id", "gridbox" + i);
@@ -12,22 +44,28 @@ function createGrid(gridSize) {
                     gridBox.style.opacity = parseFloat(gridBox.style.opacity, 10) +0.1;
                 }
             });
-            container.appendChild(gridBox);
+            gridContainer.appendChild(gridBox);
         }
     }
 // }
 
+const resetGrid = document.getElementById("submit");
+const gridSizeInput = document.getElementById("gridSizeInput");
+gridSizeInput.focus();
+resetGrid.addEventListener('click', () => {
+    if (gridSizeInput.value > 128 || gridSizeInput.value < 16){
+        gridContainer.innerHTML = '';
+        gridSizeInput.value = "";
+        gridSizeInput.placeholder = "Grid size must be between 16 and 128. Reseting grid to 16.";
+        createGrid(16);
+    } else {
+    gridContainer.innerHTML = '';
+    createGrid(gridSizeInput.value);
+    gridSizeInput.value = '';
+    gridSizeInput.placeholder = "Enter a grid size (16-128)"
+    }
+})
 
-function resetSketchPad(newGridSize) {
-    let body = document.querySelector('body');
-    let container = document.createElement("div");
-    container.setAttribute("id", "container");
-    container.style.cssText = `display: flex;flex-wrap: wrap;border-radius: 10px;border: ridge 5px lightgrey;margin: 0 auto; padding: 0; height: 800px;width: 800px`;
-    body.appendChild(container);
-    let gridSize = newGridSize;
-    createGrid(gridSize);
-}
 
-resetSketchPad(64);
 // currently on step three of this site.
 // https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project?ref=lnav
